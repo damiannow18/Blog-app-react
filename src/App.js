e5cf5a3154858a6954615d904d325f8becae2cfd
damiannow18 <73836865+damiannow18@users.noreply.github.com>
2022-04-16 18:02:40 +0200

    Implement authentication

--- src/App.js
+++ src/App.js
@@ -4,19 +4,31 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import CreatePost from './pages/CreatePost';
 import Info from './pages/Info';
+import { useState } from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from './firebase-config';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const signOutUser = () => {
+    signOut(auth).then(() => {
+      localStorage.clear();
+      setIsLoggedIn(false);
+      window.location.pathname = "/login";
+    })
+  }
   return (
   <Router>
     <nav>
       <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
-      <Link to="/create-post">Create Post</Link>
+      {isLoggedIn && <Link to="/create-post">Create Post</Link>}
       <Link to="/info">Info</Link>
+      {!isLoggedIn ? <Link to="/login">Login</Link> : <button onClick={signOutUser}>Log out</button> }
     </nav>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn}/>} />
         <Route path="/create-post" element={<CreatePost/>} />
         <Route path="/info" element={<Info/>} />
       </Routes>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { auth, provider } from '../firebase-config';
+import { signInWithPopup } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
-function Login() {
-    return <div>LOGIN</div>;
+function Login({setIsLoggedIn}) {
+
+  let navigate = useNavigate();
+
+  const signInWithGoogle = () => {
+     signInWithPopup(auth, provider).then((result) => {
+       localStorage.setItem("isLoggedIn", true);
+      setIsLoggedIn(true);
+      navigate("/");
+     }); 
+  };
+    return (
+    <div className='loginPage'>
+      <p>Sign in to continue</p>
+      <button 
+        className='login-with-google-btn' 
+        onClick={signInWithGoogle}
+      >
+        Sign in with Google
+      </button>
+      </div>
+    );
 }
 
 export default Login;
