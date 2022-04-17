5e7711dc60bec09a34a75c0ac66608f4864c3dca
damiannow18 <73836865+damiannow18@users.noreply.github.com>
2022-04-17 09:42:45 +0200

    Add css and create post container

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
 function CreatePost() {
-    return <div>CreatePost</div>;
+    return ( 
+    <div className='createPostPage'>
+      <div className='createPostContainer'>
+      <h1>Create a post</h1>
+        <div className='inputGroup'>
+          <label>Title: </label>
+          <input/>
+        </div>
+        <div className='inputGroup'>
+          <label>Content: </label>
+          <textarea/>
+        </div>
+        <button>Submit post</button>
+      </div> 
+    </div>
+    );
 }
 
 export default CreatePost;
